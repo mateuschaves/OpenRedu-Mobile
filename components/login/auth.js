c53bf23611a85239b53c111042689f630aada979
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-02-25 21:31:04 -0300

    Melhoria no código

--- components/login/auth.js
+++ components/login/auth.js
@@ -8,6 +8,7 @@ import React, { Component } from 'react';
 import {
   Platform,
   WebView,
+  AsyncStorage,
 } from 'react-native';
 
 import axios from 'axios';
@@ -15,45 +16,50 @@ import { StackNavigator } from 'react-navigation';
 
 type Props = {};
 const app = {
-  client_id: 'qHnf1X6EXNnx5Z9DeyAvPRO72ndV8xPsSvbv4uLe',
-  grant_type: 'authorization_code',
-  client_secret: 'wFlRombfhPcYm96cDHrgOd80udgEAM3Dq8CgrOk1',
+  client_id:      'qHnf1X6EXNnx5Z9DeyAvPRO72ndV8xPsSvbv4uLe',
+  grant_type:     'authorization_code',
+  client_secret:  'wFlRombfhPcYm96cDHrgOd80udgEAM3Dq8CgrOk1',
   redirect_uri:   'https://github.com/mateuschaves'
 }
 
 export default class Auth extends Component<Props> {
   state = {
-    control_post: 0,
+    control_redirect: 0,
   }
   _changeScreen(params){
     this.props.navigation.navigate('InitAuth', {
         params: params
     });
   }
-  _onNavigationStateChange(webViewState) {
+   _onNavigationStateChange(webViewState) {
     let url = webViewState.url;
     let index_code = url.match("code");
     let code = '';
-    let check_url = url.match("openredu");
-    let token = '';
+    let check_url = url.match("oauth");
     if(!check_url){
        code = url.substring(37,57);
        params = {
            app: app,
            code: code
        }
-       this._changeScreen(params);
+       if(this.state.control_redirect == 0){
+         this.setState({control_redirect: 1});
+          this._changeScreen(params);
+       }
     }
     if(code && this.state.control_post == 0){
-        this.state.control_post++;
+        this.setState({control_post: 1});
         axios.post(`https://openredu.ufpe.br/oauth/token?code=${code}&client_id=${app.client_id}&grant_type=${app.grant_type}&client_secret=${app.client_secret}&redirect_uri=${app.redirect_uri}`,{
         }).then(function(response){
-          token = response.data.access_token;
-          if(token != ''){
+          try {
+            AsyncStorage.setItem('@OpenRedu:token', response.data.access_token);
             this.props.navigation.navigate('InitAuth');
+          } catch (error){
+            console.log(error);
           }
         })
         .catch(function(error){
+          console.log(error);
         });
     }
   }
--- components/login/init.js
+++ components/login/init.js
@@ -1,16 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
   Text,
   View,
-  Button
+  Button,
+  AsyncStorage
 } from 'react-native';
 
 import { StackNavigator } from 'react-navigation';
@@ -18,31 +13,34 @@ import axios from 'axios';
 
 export default class Init extends Component<Props> {
     state = {
+        // Evita que o código seja executado duas vezes.
+        // Gambiarra de um bug desconhecido.
         control_request: 0,
+        // Parâmetros passados pelo auth.js.
+        params: this.props.navigation.state.params,
     }
-    _getToken(params){
-        console.log('CONTROL REQUEST ANTES DE SER MUDADO : ' + this.state.control_request);
-        if(params && this.state.control_request != 1){
+      componentDidMount(){
+        if(this.state.params && this.state.control_request == 0){
             this.setState({control_request: 1});
-        console.log('CONTROL REQUEST DEPOIS DE SER MUDADO : ' + this.state.control_request);
-            
-            console.log("código " + params.params.code);
-            let code            = params.params.code;
-            let client_secret   = params.params.app.client_secret;
-            let client_id       = params.params.app.client_id;
-            let redirect_uri    = params.params.app.redirect_uri;
-            let grant_type      = params.params.app.grant_type;
-            axios.post(`https://openredu.ufpe.br/oauth/token?code=${code}&client_id=${client_id}&grant_type=${grant_type}&client_secret=${client_secret}&redirect_uri=${redirect_uri}`).then(function(response){
-              token = response.data.access_token;
-              console.log(' token ' + token);
+            let { params:{ code },
+                  params:{app:{client_id}},
+                  params:{app:{client_secret}},
+                  params:{app:{grant_type}},
+                  params:{app:{redirect_uri}}
+                  } = this.state.params;
+            axios.post('https://openredu.ufpe.br/oauth/token?', {
+                client_id:      client_id,
+                client_secret:  client_secret,
+                grant_type:     grant_type,
+                redirect_uri:   redirect_uri,
+                code:           code,
+            }).then(function(response){
             })
             .catch(function(error){
             });
         }
     }
   render() {
-    const {params} = this.props.navigation.state;
-    this._getToken(params);
     return (
       <View>
           <Text style={styles.textLogin}> Login </Text>
